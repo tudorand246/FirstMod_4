699ae3335d655b83814493adffdecbb203f14fd4
ItsAMeMurio <90408884+ItsAMeMurio@users.noreply.github.com>
2022-04-04 18:50:13 -0500

    began adding test item implementation

--- src/main/java/com/qcmbrs/firstmod/FirstMod.java
+++ src/main/java/com/qcmbrs/firstmod/FirstMod.java
@@ -1,9 +1,11 @@
 package com.qcmbrs.firstmod;
 
+import com.qcmbrs.firstmod.item.ModItems;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -28,9 +30,12 @@ public class FirstMod
 
     public FirstMod()
     {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register ourselves for server and other game events we are interested in
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 
---
+++ src/main/java/com/qcmbrs/firstmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.qcmbrs.firstmod.item;
+
+import com.qcmbrs.firstmod.FirstMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> PARM = ITEMS.register("p_arm",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus)
+    {
+        ITEMS.register(eventBus);
+    }
+}
